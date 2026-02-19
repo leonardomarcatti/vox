028dff964be461234ec8bad9187796699bccbfd5
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2026-02-19 15:37:07 -0300

    Logup/Login components

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,7 @@
-const Logup = () => {
+const login = () => {
    return <div className="shadow-lg rounded-3 col-5 p-4 bg-body-tertiary">
-      <h2 className='mb-3'>Cadastre-se</h2>
+      <h2 className='mb-3'>Login</h2>
       <form action="" method="post">
-         <div className="mb-3 text-start">
-            <label htmlFor="userName" className="form-label">Nome de Usuário:</label>
-            <input type="text" name="userName" id="userName" className="form-control" />
-         </div>
          <div className="mb-3 text-start">
             <label htmlFor="email" className="form-label">Email:</label>
             <input type="email" name="email" id="email" className="form-control" />
@@ -14,16 +10,12 @@ const Logup = () => {
             <label htmlFor="senha" className="form-label">Senha:</label>
             <input type="password" name="password" id="password" className="form-control" />
          </div>
-         <div className="mb-3  text-start">
-            <label htmlFor="password_confirm" className="form-label">Repita Senha:</label>
-            <input type="email" name="password_confirm" id="password_confirm" className="form-control" />
-         </div>
          <span className=''>
-            <button type="submit" className='btn btn-primary mx-2'>Cadastrar <i class="fa-solid fa-user-plus"></i></button>
+            <button type="submit" className='btn btn-primary mx-2'>Login <i class="fa-solid fa-lock-open"></i></button>
             <button type="reset" className='btn btn-warning mx-2'>Limpar <i class="fa-solid fa-x"></i></button>
          </span>
       </form>
-   </div> 
+   </div>
 }
 
-export default Logup
+export default login
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,12 +1,15 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Home from '../pages/Home'
 import Layout from '../layout/Layout'
+import Logup from '../components/Logup'
+import Login from '../components/Login'
 
 const routes = createBrowserRouter([
    {path: '/', element: <Layout />, children: [
       {index: true, element: <Home/>}
    ]},
-   {path: 'logup', element:}
+   {path: 'logup', element: <Logup />},
+   {path: 'login', element: <Login />}
 ])
 
 export default routes
